7d6acd0029a17b4528881128ce6affbe94d738a9
Diegopalma123 <diegito.palma1245@gmail.com>
2026-06-23 20:22:35 -0500

    Update ClienteController.java

--- backend/backend/src/main/java/com/agenciaviajes/backend/controller/ClienteController.java
+++ backend/backend/src/main/java/com/agenciaviajes/backend/controller/ClienteController.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/clientes")
 public class ClienteController {
     private final ClienteService service;
